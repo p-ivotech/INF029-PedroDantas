c5e8e59510a0258b0ab68a0b369847d4c09fc2c5
p-ivotech <pedroivodantas@gmail.com>
2026-01-13 15:36:51 -0300

    teste script

--- trabalho2/trabalho2.h
+++ trabalho2/trabalho2.h
@@ -1,5 +1,28 @@
-#ifndef ITEM_H
-#define ITEM_H
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome: Pedro Ivo Alves da Silva Dantas
+//  email: pedroivodantas@gmail.com 
+//  Matrícula: 20251160046
+//  Semestre: 2º semestre
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 07/05/2021 - 19/08/2016 - 13/01/2025
+
+// #################################################
+
+
+#ifndef trabalho2_H
+#define trabalho2_H
 
 #define TAM_PADRAO 100
 #define TAM_VP 10
